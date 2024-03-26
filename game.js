5bf09cfb820dc3587ce578facea7dc8233a39ddf
Marko Ristin <marko@ristin.ch>
2024-03-26 12:49:03 +0100

    Split middle adges (#19)
    The level took to long, so we split it in half.

--- game.js
+++ game.js
@@ -985,7 +985,7 @@ const levels = [
         ]
     },
     {
-        name: "Middle ages",
+        name: "Middle ages 1",
         vocabulary: [
             {
                 question: "<img src='additional-emojis/castle.png'>",
@@ -1086,7 +1086,12 @@ const levels = [
             {
                 question: "<img src='additional-emojis/sabre.png'>",
                 answers: ["sabre", "saber", "sable"]
-            },
+            }
+        ]
+    },
+    {
+        name: "Middle ages 2",
+        vocabulary: [
             {
                 question: "<img src='additional-emojis/katana.png'>",
                 answers: ["katana"]
